07c79b2d2c9424006d2e880791bb83ff843f136a
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-01-24 18:36:41 +0600

    Header added and responsive

---
+++ dev-diary-client/src/components/Header.jsx
@@ -0,0 +1,53 @@
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+import { Link, useLocation } from "react-router-dom";
+
+export const Header = () => {
+    const path = useLocation().pathname;
+    return (
+        <Navbar className="border-b-2">
+            <Link to="/" className="text-xl font-bold">
+                Dev Diary
+            </Link>
+
+            <form>
+                <TextInput
+                    type="text"
+                    placeholder="Search..."
+                    rightIcon={AiOutlineSearch}
+                    className="hidden lg:inline"
+                />
+            </form>
+            <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+                <AiOutlineSearch />
+            </Button>
+
+            <div className="flex gap-2 order-2">
+                <Button
+                    className="hidden sm:inline w-12 h-10"
+                    color="gray"
+                    pill
+                >
+                    <FaMoon />
+                </Button>
+                <Link to="/sign-in">
+                    <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+                </Link>
+                <Navbar.Toggle />
+            </div>
+
+            <Navbar.Collapse>
+                <Navbar.Link active={path === "/"} as={"div"}>
+                    <Link to="/">Home</Link>
+                </Navbar.Link>
+                <Navbar.Link active={path === "/about"} as={"div"}>
+                    <Link to="/about">About</Link>
+                </Navbar.Link>
+                <Navbar.Link active={path === "/projects"} as={"div"}>
+                    <Link to="/projects">Projects</Link>
+                </Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
--- dev-diary-client/tailwind.config.js
+++ dev-diary-client/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import { Flowbite } from "flowbite-react";
+("flowbite-react");
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "node_modules/flowbite-react/lib/esm/**/*.js",
+    ],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [Flowbite],
 };
