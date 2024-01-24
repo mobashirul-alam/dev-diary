aa6ac2e09bf5fcf99682d947a29cc9fd02eed66e
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-01-24 21:32:47 +0600

    Database connected

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 
+dotenv.config();
+mongoose
+    .connect(process.env.DATABASE_URL)
+    .then(() => {
+        console.log("Mongodb is connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 const app = express();
 
 app.listen(3000, () => {
