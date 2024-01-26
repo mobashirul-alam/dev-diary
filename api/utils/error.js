4c7361321f5f5e92f9ca7321b590343ba144cb81
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-01-26 09:58:10 +0600

    Middleware function created for handling error.

---
+++ api/utils/error.js
@@ -0,0 +1,9 @@
+export const errorHandler = (statusCode, message) => {
+    const error = new Error();
+
+    error.success = false;
+    error.statusCode = statusCode;
+    error.message = message;
+
+    return error;
+};
