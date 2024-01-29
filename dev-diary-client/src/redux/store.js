4d0018b0700802082ccb3b4142253de8b42d57b6
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-01-29 20:12:44 +0600

    Redux Persist added to save user info

--- dev-diary-client/src/redux/store.js
+++ dev-diary-client/src/redux/store.js
@@ -1,8 +1,44 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+    FLUSH,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+    REHYDRATE,
+    persistReducer,
+    persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user/userSlice";
 
-export const store = configureStore({
-    reducer: {
-        user: userReducer,
-    },
+const rootReducer = combineReducers({
+    user: userReducer,
 });
+
+const persistConfig = {
+    key: "root",
+    storage,
+    version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
+});
+
+export const persistor = persistStore(store);
