a708207177bd9175aeb644c2632b80ab1de76333
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-02-03 21:40:21 +0600

    Update api route created.

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
@@ -20,6 +21,7 @@ app.listen(3000, () => {
 });
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
