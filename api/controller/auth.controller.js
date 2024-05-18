d8acf20a27fc60836d42ee32e9c15f71f965ecc4
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-05-18 09:03:20 +0600

    Admin functionality added in user

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -56,7 +56,10 @@ export const signin = async (req, res, next) => {
 
         const { password: pass, ...rest } = validUser._doc;
 
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const token = jwt.sign(
+            { id: validUser._id, isAdmin: validUser.isAdmin },
+            process.env.JWT_SECRET
+        );
 
         res.status(200)
             .cookie("access_token", token, { httpOnly: true })
@@ -73,7 +76,10 @@ export const google = async (req, res, next) => {
         const user = await User.findOne({ email });
 
         if (user) {
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const token = jwt.sign(
+                { id: user._id, isAdmin: user.isAdmin },
+                process.env.JWT_SECRET
+            );
             const { password, ...rest } = user._doc;
             res.status(200)
                 .cookie("access_token", token, { httpOnly: true })
@@ -93,7 +99,10 @@ export const google = async (req, res, next) => {
             });
 
             await newUser.save();
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const token = jwt.sign(
+                { id: user._id, isAdmin: newUser.isAdmin },
+                process.env.JWT_SECRET
+            );
             const { password, ...rest } = newUser._doc;
             res.status(200)
                 .cookie("access_token", token, { httpOnly: true })
