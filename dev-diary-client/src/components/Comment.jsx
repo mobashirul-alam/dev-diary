667e9cc8e89f4171bf4ba57884828ee8d541eb00
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-05-24 20:34:41 +0600

    comment delete functionality implemented.

--- dev-diary-client/src/components/Comment.jsx
+++ dev-diary-client/src/components/Comment.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { FaThumbsUp } from "react-icons/fa";
 import { useSelector } from "react-redux";
 
-const Comment = ({ comment, onLike, onEdit }) => {
+const Comment = ({ comment, onLike, onEdit, onDelete }) => {
     const [user, setUser] = useState({});
     const [isEditing, setIsEditing] = useState(false);
     const [editedContent, setEditedContent] = useState(comment.content);
@@ -120,13 +120,24 @@ const Comment = ({ comment, onLike, onEdit }) => {
                             {currentUser &&
                                 (currentUser._id === comment.userId ||
                                     currentUser.isAdmin) && (
-                                    <button
-                                        type="button"
-                                        className="text-gray-400 hover:text-blue-500"
-                                        onClick={handleEdit}
-                                    >
-                                        Edit
-                                    </button>
+                                    <>
+                                        <button
+                                            type="button"
+                                            className="text-gray-400 hover:text-blue-500"
+                                            onClick={handleEdit}
+                                        >
+                                            Edit
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="text-gray-400 hover:text-red-500"
+                                            onClick={() =>
+                                                onDelete(comment._id)
+                                            }
+                                        >
+                                            Delete
+                                        </button>
+                                    </>
                                 )}
                         </div>
                     </>
--- dev-diary-client/src/components/CommentSection.jsx
+++ dev-diary-client/src/components/CommentSection.jsx
@@ -1,5 +1,6 @@
-import { Alert, Button, Textarea } from "flowbite-react";
+import { Alert, Button, Modal, Textarea } from "flowbite-react";
 import { useEffect, useState } from "react";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import Comment from "./Comment";
@@ -9,6 +10,8 @@ const CommentSection = ({ postId }) => {
     const [comment, setComment] = useState("");
     const [comments, setComments] = useState([]);
     const [commentError, setCommentError] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [commentToDelete, setCommentToDelete] = useState(null);
     const { currentUser } = useSelector((state) => state.user);
 
     const navigate = useNavigate();
@@ -99,6 +102,25 @@ const CommentSection = ({ postId }) => {
             )
         );
     };
+    const handleDelete = async (commentId) => {
+        setShowModal(false);
+        try {
+            if (!currentUser) {
+                navigate("/sign-in");
+                return;
+            }
+            const res = await fetch(`/api/comment/deleteComment/${commentId}`, {
+                method: "DELETE",
+            });
+            if (res.ok) {
+                setComments(
+                    comments.filter((comment) => comment._id !== commentId)
+                );
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
     return (
         <div className="max-w-2xl mx-auto w-full p-3">
             {currentUser ? (
@@ -176,10 +198,45 @@ const CommentSection = ({ postId }) => {
                             comment={comment}
                             onLike={handleLike}
                             onEdit={handleEdit}
+                            onDelete={(commentId) => {
+                                setShowModal(true);
+                                setCommentToDelete(commentId);
+                            }}
                         />
                     ))}
                 </>
             )}
+
+            <Modal
+                show={showModal}
+                size="md"
+                onClose={() => setShowModal(false)}
+                popup
+            >
+                <Modal.Header />
+                <Modal.Body>
+                    <div className="text-center">
+                        <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                        <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                            Are you sure you want to delete this post?
+                        </h3>
+                        <div className="flex justify-center gap-4">
+                            <Button
+                                color="failure"
+                                onClick={() => handleDelete(commentToDelete)}
+                            >
+                                {"Yes, I'm sure"}
+                            </Button>
+                            <Button
+                                color="gray"
+                                onClick={() => setShowModal(false)}
+                            >
+                                No, cancel
+                            </Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
         </div>
     );
 };
